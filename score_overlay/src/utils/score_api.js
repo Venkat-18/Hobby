40a941ee75ebf188f8d72224a763dd9f07022f2c
unknown <venkatesh18596@gmail.com>
2019-06-11 15:44:37 +0530

    Using fetch to get scores

--- score_overlay/src/utils/score_api.js
+++ score_overlay/src/utils/score_api.js
@@ -1,8 +1,26 @@
-const { net } = require('electron')
+// const { net } = require('electron')
 
 export default class Score_api {
   test() {
-    // const req = new ClientRequest("https://reqres.in/api/users?page=2")
+    fetch('https://reqres.in/api/users?page=2')
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+
     return "Test"
   }
 
